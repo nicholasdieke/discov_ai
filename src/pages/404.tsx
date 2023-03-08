e506942ac9a94f76deac809f9b5d06fe1d2a44f8
nicholasdieke <nicholasdieke@icloud.com>
2023-03-09 00:40:34 +0100

    cleaned up wrong id and 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,9 @@
-import Head from 'next/head'
-import {ErrorComponent} from "@blitzjs/next"
+import { Routes } from "@blitzjs/next"
+import { Box, Button, Flex, Heading } from "@chakra-ui/react"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import "react-datepicker/dist/react-datepicker.css"
+import Header from "src/core/components/Header"
 
 // ------------------------------------------------------
 // This page is rendered if a route match is not found
@@ -7,14 +11,54 @@ import {ErrorComponent} from "@blitzjs/next"
 export default function Page404() {
   const statusCode = 404
   const title = "This page could not be found"
+  const [photoUrl, setPhotoUrl] = useState("")
+  const router = useRouter()
+
+  const getPhoto = () => {
+    fetch("/api/getHomePhotos")
+      .then((response) => response.json())
+      .then((response) => {
+        const random = Math.floor(Math.random() * 10)
+        setPhotoUrl(response.result[random].urls.full || "")
+      })
+      .catch((e) => console.log(e))
+  }
+
+  useEffect(() => {
+    getPhoto()
+  }, [])
+
   return (
-    <>
-      <Head>
-        <title>
-          {statusCode}: {title}
-        </title>
-      </Head>
-      <ErrorComponent statusCode={statusCode} title={title} />
-    </>
+    <Box
+      // className="App"
+      backgroundColor="#0F1014"
+      h="100%"
+      minH="100vh"
+      color="white"
+      overflow="hidden"
+    >
+      <Box h="100%" minH="100vh" px={{ base: "2rem", lg: "7.5rem" }}>
+        <Header theme="white" />
+        <Flex
+          alignItems={"center"}
+          justifyContent="center"
+          mt={{ base: "1rem", md: "6rem" }}
+          w="100%"
+          flexDir="column"
+        >
+          <Heading color="white" size="lg" textAlign="center" mt="5rem" mb="1rem">
+            Sorry, this page does not exist.
+          </Heading>
+          <Button
+            colorScheme="purple"
+            mt="1rem"
+            width="200px"
+            onClick={() => router.push(Routes.Home())}
+          >
+            Go Home!
+          </Button>
+        </Flex>
+      </Box>
+    </Box>
   )
 }
--- src/pages/trip/[id].tsx
+++ src/pages/trip/[id].tsx
@@ -1,4 +1,4 @@
-import { BlitzPage } from "@blitzjs/next"
+import { BlitzPage, Routes } from "@blitzjs/next"
 import { invoke } from "@blitzjs/rpc"
 import {
   Accordion,
@@ -36,9 +36,10 @@ import getTrip from "src/core/queries/getTrip"
 
 const TripPage: BlitzPage = () => {
   const router = useRouter()
-  const tripId = router.query.id
+  const tripId = router.query.id || ""
+  const [loading, setLoading] = useState(true)
 
-  const [myTrip, setMyTrip] = useState<Trip | null>(null)
+  const [myTrip, setMyTrip] = useState<Trip | null | undefined>(undefined)
 
   const monthNames = [
     "january",
@@ -68,8 +69,10 @@ const TripPage: BlitzPage = () => {
     // Gets the info from db and then gets an image from unsplash
     invoke(getTrip, { id: tripId })
       .then((trip) => {
+        console.log(trip)
         setMyTrip(trip as Trip)
         getPhoto(trip?.destination || "city")
+        setLoading(false)
       })
       .catch((e) => console.log(e))
   }
@@ -85,169 +88,195 @@ const TripPage: BlitzPage = () => {
   }
 
   return (
-    <Box backgroundColor="#0F1014" color="white">
-      {!!myTrip && (
-        <Box px={{ base: "2rem", lg: "7.5rem" }} py={{ base: "0.25rem", lg: "0.5rem" }}>
-          <Header theme="white" />
-          <Flex
-            h={{ base: "200px", lg: "300px" }}
-            bgPos="center"
-            bgRepeat="no-repeat"
-            bgImage={photoUrl}
-            bgSize="cover"
-            borderRadius="10px"
-          >
+    <Box backgroundColor="#0F1014" h="100%" minH="100vh" color="white">
+      <Box px={{ base: "2rem", lg: "7.5rem" }} py={{ base: "0.25rem", lg: "0.5rem" }}>
+        <Header theme="white" />
+        {!loading && !!myTrip && (
+          <>
             <Flex
-              p="1rem"
-              alignItems="center"
-              justifyContent="center"
-              h={{ base: "200px", lg: "300px" }}
-              bgColor="rgb(0,0,0,0.5)"
-              w="100%"
+              h={{ base: "200px", lg: "250px" }}
+              bgPos="center"
+              bgRepeat="no-repeat"
+              bgImage={photoUrl}
+              bgSize="cover"
+              borderRadius="10px"
             >
-              <VStack spacing="1rem" color="white" textAlign="center">
-                <Heading fontSize={{ base: "40px", lg: "70px" }}>{myTrip.destination}</Heading>
-                <Divider />
-                <HStack fontSize="18px">
-                  <FontAwesomeIcon icon={faCalendarDays} size="1x" />
-                  <Text>
-                    {myTrip.daterange[0]?.toLocaleDateString("en-US", dateOptions)} -{" "}
-                    {myTrip.daterange[1]?.toLocaleDateString("en-US", dateOptions)}
-                  </Text>
-                </HStack>
-              </VStack>
-            </Flex>
-          </Flex>
-
-          <Box mt="2rem" mb="5rem">
-            <Heading size="md" mb="1rem">
-              Before You Go
-            </Heading>
-            <Flex justifyContent="space-between">
-              <Flex gap="1rem" flexDir={{ base: "column", md: "row" }} mb="2rem">
-                <a href="https://www.skyscanner.net/" target="_blank" rel="noreferrer">
-                  <Button
-                    leftIcon={<FontAwesomeIcon icon={faPlaneDeparture} size="1x" />}
-                    rightIcon={<FontAwesomeIcon icon={faChevronRight} size="1x" />}
-                    variant="outline"
-                  >
-                    See Flights
-                  </Button>
-                </a>
-
-                <a href="https://www.booking.com/" target="_blank" rel="noreferrer">
-                  <Button
-                    leftIcon={<FontAwesomeIcon icon={faBed} size="1x" />}
-                    rightIcon={<FontAwesomeIcon icon={faChevronRight} size="1x" />}
-                    variant="outline"
-                  >
-                    See Stays
-                  </Button>
-                </a>
-                <a
-                  href={
-                    "https://www.meteoprog.com/weather/" +
-                    myTrip.destination.replaceAll(" ", "") +
-                    "/month/" +
-                    monthNames[(myTrip.daterange[0] as Date).getMonth()]
-                  }
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <Button
-                    leftIcon={<FontAwesomeIcon icon={faCloudSun} size="1x" />}
-                    rightIcon={<FontAwesomeIcon icon={faChevronRight} size="1x" />}
-                    variant="outline"
-                  >
-                    See Weather
-                  </Button>
-                </a>
-              </Flex>
-
-              <Button
-                leftIcon={<FontAwesomeIcon icon={faClipboard} size="1x" />}
-                onClick={() =>
-                  navigator.clipboard.writeText(
-                    "Check out my trip made on DiscovAI! www.discovai.com" + router.asPath
-                  )
-                }
-                variant="outline"
-              >
-                Copy Trip
-              </Button>
-            </Flex>
-
-            <Heading size="md" mb="1rem">
-              Your Personalised Itinerary
-            </Heading>
-            <VStack spacing="3rem">
-              <Accordion
-                defaultIndex={Array.from(
-                  { length: myTrip.itinerary.split("Day").slice(1).length },
-                  (_, i) => i
-                )}
-                allowMultiple
+              <Flex
+                p="1rem"
+                alignItems="center"
+                justifyContent="center"
+                h={{ base: "200px", lg: "250px" }}
+                bgColor="rgb(0,0,0,0.5)"
                 w="100%"
               >
-                {myTrip.itinerary
-                  .split("Day")
-                  .slice(1)
-                  .map((day, index) => (
-                    <AccordionItem key={"Trip-Day" + index}>
-                      <AccordionButton>
-                        <Box
-                          as="span"
-                          fontWeight="700"
-                          fontSize="18px"
-                          py="1rem"
-                          w="100%"
-                          flex="1"
-                          textAlign="left"
-                        >
-                          Day {index + 1}
-                        </Box>
-                        <AccordionIcon />
-                      </AccordionButton>
+                <VStack spacing="1rem" color="white" textAlign="center">
+                  <Heading fontSize={{ base: "40px", lg: "70px" }}>{myTrip.destination}</Heading>
+                  <Divider />
+                  <HStack fontSize="18px">
+                    <FontAwesomeIcon icon={faCalendarDays} size="1x" />
+                    <Text>
+                      {myTrip.daterange[0]?.toLocaleDateString("en-US", dateOptions)} -{" "}
+                      {myTrip.daterange[1]?.toLocaleDateString("en-US", dateOptions)}
+                    </Text>
+                  </HStack>
+                </VStack>
+              </Flex>
+            </Flex>
 
-                      <AccordionPanel>
-                        <VStack spacing="1rem">
-                          <Flex alignItems="start" flexDir={{ base: "column", md: "row" }}>
-                            <HStack className="dayTimeBox" mb="1rem">
-                              <FontAwesomeIcon icon={faMugSaucer} size="1x" />
-                              <Text>Morning</Text>
-                            </HStack>
-                            <Text fontSize="18px" pl={{ base: "0rem", md: "8rem" }}>
-                              {day.substring(3).split("Morning:")[1]?.split("Afternoon:")[0]}
-                            </Text>
-                          </Flex>
-                          <Flex alignItems="start" flexDir={{ base: "column", md: "row" }}>
-                            <HStack className="dayTimeBox" mb="1rem">
-                              <FontAwesomeIcon icon={faSun} size="1x" />
-                              <Text>Afternoon</Text>
-                            </HStack>
-                            <Text fontSize="18px" pl={{ base: "0rem", md: "8rem" }}>
-                              {day.substring(3).split("Afternoon:")[1]?.split("Evening:")[0]}
-                            </Text>
-                          </Flex>
-                          <Flex alignItems="start" flexDir={{ base: "column", md: "row" }}>
-                            <HStack className="dayTimeBox" mb="1rem">
-                              <FontAwesomeIcon icon={faMoon} size="1x" />
-                              <Text>Evening</Text>
-                            </HStack>
+            <Flex mt="2rem" mb="5rem" flexDir={{ base: "column-reverse", md: "column" }}>
+              <Box>
+                <Heading size="md" my="1rem">
+                  Before You Go
+                </Heading>
+                <Flex justifyContent="space-between">
+                  <Flex gap="1rem" flexDir={{ base: "column", md: "row" }} mb="2rem">
+                    <a href="https://www.skyscanner.net/" target="_blank" rel="noreferrer">
+                      <Button
+                        leftIcon={<FontAwesomeIcon icon={faPlaneDeparture} size="1x" />}
+                        rightIcon={<FontAwesomeIcon icon={faChevronRight} size="1x" />}
+                        variant="outline"
+                      >
+                        See Flights
+                      </Button>
+                    </a>
 
-                            <Text fontSize="18px" pl={{ base: "0rem", md: "8rem" }}>
-                              {day.substring(3).split("Evening:")[1]}
-                            </Text>
-                          </Flex>
-                        </VStack>
-                      </AccordionPanel>
-                    </AccordionItem>
-                  ))}
-              </Accordion>
-            </VStack>
-          </Box>
-        </Box>
-      )}
+                    <a href="https://www.booking.com/" target="_blank" rel="noreferrer">
+                      <Button
+                        leftIcon={<FontAwesomeIcon icon={faBed} size="1x" />}
+                        rightIcon={<FontAwesomeIcon icon={faChevronRight} size="1x" />}
+                        variant="outline"
+                      >
+                        See Stays
+                      </Button>
+                    </a>
+                    <a
+                      href={
+                        "https://www.meteoprog.com/weather/" +
+                        myTrip.destination.replaceAll(" ", "") +
+                        "/month/" +
+                        monthNames[(myTrip.daterange[0] as Date).getMonth()]
+                      }
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <Button
+                        leftIcon={<FontAwesomeIcon icon={faCloudSun} size="1x" />}
+                        rightIcon={<FontAwesomeIcon icon={faChevronRight} size="1x" />}
+                        variant="outline"
+                      >
+                        See Weather
+                      </Button>
+                    </a>
+                  </Flex>
+
+                  <Button
+                    leftIcon={<FontAwesomeIcon icon={faClipboard} size="1x" />}
+                    onClick={() =>
+                      navigator.clipboard.writeText(
+                        "Check out my trip made on DiscovAI! www.discovai.com" + router.asPath
+                      )
+                    }
+                    variant="outline"
+                  >
+                    Copy Trip
+                  </Button>
+                </Flex>
+              </Box>
+              <Box>
+                <Heading size="md" mb="1rem">
+                  Your Personalised Itinerary
+                </Heading>
+                <VStack spacing="3rem">
+                  <Accordion
+                    defaultIndex={Array.from(
+                      { length: myTrip.itinerary.split("Day").slice(1).length },
+                      (_, i) => i
+                    )}
+                    allowMultiple
+                    w="100%"
+                  >
+                    {myTrip.itinerary
+                      .split("Day")
+                      .slice(1)
+                      .map((day, index) => (
+                        <AccordionItem key={"Trip-Day" + index}>
+                          <AccordionButton>
+                            <Box
+                              as="span"
+                              fontWeight="700"
+                              fontSize="18px"
+                              py="1rem"
+                              w="100%"
+                              flex="1"
+                              textAlign="left"
+                            >
+                              Day {index + 1}
+                            </Box>
+                            <AccordionIcon />
+                          </AccordionButton>
+
+                          <AccordionPanel>
+                            <VStack spacing="1rem">
+                              <Flex alignItems="start" flexDir={{ base: "column", md: "row" }}>
+                                <HStack className="dayTimeBox" mb="1rem">
+                                  <FontAwesomeIcon icon={faMugSaucer} size="1x" />
+                                  <Text>Morning</Text>
+                                </HStack>
+                                <Text fontSize="18px" pl={{ base: "0rem", md: "8rem" }}>
+                                  {day.substring(3).split("Morning:")[1]?.split("Afternoon:")[0]}
+                                </Text>
+                              </Flex>
+                              <Flex alignItems="start" flexDir={{ base: "column", md: "row" }}>
+                                <HStack className="dayTimeBox" mb="1rem">
+                                  <FontAwesomeIcon icon={faSun} size="1x" />
+                                  <Text>Afternoon</Text>
+                                </HStack>
+                                <Text fontSize="18px" pl={{ base: "0rem", md: "8rem" }}>
+                                  {day.substring(3).split("Afternoon:")[1]?.split("Evening:")[0]}
+                                </Text>
+                              </Flex>
+                              <Flex alignItems="start" flexDir={{ base: "column", md: "row" }}>
+                                <HStack className="dayTimeBox" mb="1rem">
+                                  <FontAwesomeIcon icon={faMoon} size="1x" />
+                                  <Text>Evening</Text>
+                                </HStack>
+
+                                <Text fontSize="18px" pl={{ base: "0rem", md: "8rem" }}>
+                                  {day.substring(3).split("Evening:")[1]}
+                                </Text>
+                              </Flex>
+                            </VStack>
+                          </AccordionPanel>
+                        </AccordionItem>
+                      ))}
+                  </Accordion>
+                </VStack>
+              </Box>
+            </Flex>
+          </>
+        )}
+        {!loading && myTrip === null && (
+          <Flex
+            alignItems={"center"}
+            justifyContent="center"
+            mt={{ base: "1rem", md: "6rem" }}
+            w="100%"
+            flexDir="column"
+          >
+            <Heading color="white" size="lg" textAlign="center" mt="5rem" mb="1rem">
+              Sorry, this trip does not exist.
+            </Heading>
+            <Button
+              colorScheme="purple"
+              mt="1rem"
+              width="200px"
+              onClick={() => router.push(Routes.Home())}
+            >
+              Go Home!
+            </Button>
+          </Flex>
+        )}
+      </Box>
     </Box>
   )
 }
