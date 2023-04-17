7065966a504337d77d5895645b5f538667ebd844
nicholasdieke <nicholasdieke@icloud.com>
2023-04-17 14:49:55 +0200

    switched to dropbox share image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,13 +29,13 @@ const Home: BlitzPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:title" content="DiscovAI" />
         <meta property="og:url" content="/" />
-        <meta property="og:image" content="https://www.discovai.com/share-image.png" />
+        <meta property="og:image" content="https://www.dropbox.com/s/hmmp4gklv03u11n/share-image.png?raw=1" />
         <meta name="twitter:title" content="DiscovAI" />
         <meta
           name="twitter:description"
           content="Discover your dream vacation with DiscovAI, the AI-powered travel planner that creates personalised itineraries based on your interests and budget. Plan your perfect trip today!"
         />
-        <meta name="twitter:image" content="https://www.discovai.com/share-image.png" />
+        <meta name="twitter:image" content="https://www.dropbox.com/s/hmmp4gklv03u11n/share-image.png?raw=1" />
         <meta name="twitter:card" content="summary_large_image" />
       </>
       <Box h="100%" pos="relative">
