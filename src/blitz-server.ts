84e2ecb40eed0a9b38852a44fb6823a5e3445040
nicholasdieke <nicholasdieke@icloud.com>
2024-10-15 21:10:32 +0200

    fix

--- src/blitz-server.ts
+++ src/blitz-server.ts
@@ -1,16 +1,21 @@
 import { AuthServerPlugin, PrismaStorage, simpleRolesIsAuthorized } from "@blitzjs/auth"
 import { setupBlitzServer } from "@blitzjs/next"
+import { RpcServerPlugin } from "@blitzjs/rpc"
 import { BlitzLogger } from "blitz"
 import db from "db"
 import { authConfig } from "./blitz-auth-config"
 
-export const { gSSP, gSP, api } = setupBlitzServer({
-  plugins: [
-    AuthServerPlugin({
-      ...authConfig,
-      storage: PrismaStorage(db),
-      isAuthorized: simpleRolesIsAuthorized,
-    }),
-  ],
-  logger: BlitzLogger({}),
-})
+const { api, getBlitzContext, useAuthenticatedBlitzContext, invoke, withBlitzAuth } =
+  setupBlitzServer({
+    plugins: [
+      AuthServerPlugin({
+        ...authConfig,
+        storage: PrismaStorage(db),
+        isAuthorized: simpleRolesIsAuthorized,
+      }),
+      RpcServerPlugin({}),
+    ],
+    logger: BlitzLogger({}),
+  })
+
+export { api, getBlitzContext, invoke, useAuthenticatedBlitzContext, withBlitzAuth }
